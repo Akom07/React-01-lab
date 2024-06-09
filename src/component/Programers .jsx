55e08a250e0b7b6813706bf33736b34ed33bd667
akom_work <akom07@gmail.com>
2024-06-09 19:16:17 +0300

    .

--- src/component/Programers .jsx
+++ src/component/Programers .jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Programers (props) {
   return (
-    <div>
+    <div className='programers ' style={{backgroundColor:props.companyName == 'tuwaiq'? 'violet':'none'}}>
         <h1 className='fName'>{props.name1}</h1>
         <ul>
            <li>{props.lang}</li>
